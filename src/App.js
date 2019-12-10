64d33dee8e3cdd202a90f7f84a6414a1d562c913
unknown <mikeitt009@gmail.com>
2019-12-10 07:10:55 -0500

    added media query for responsiveness

--- src/App.js
+++ src/App.js
@@ -28,39 +28,67 @@ function App() {
       <article className="project-one">
         <h1 style={{ fontSize: 35 }}>Project 1</h1>
         <div className="project-row">
-          <img src={require("./img/calculate2.jpg")} width={200} />
-          <img src={require("./img/calculate3.jpg")} width={200} />
-          <img src={require("./img/calculate1.jpg")} width={200} />
+          <img
+            src={require("./img/calculate2.jpg")}
+            width={200}
+            style={{ paddingBottom: 10 }}
+          />
+          <img
+            src={require("./img/calculate3.jpg")}
+            width={200}
+            style={{ paddingBottom: 10 }}
+          />
+          <img
+            src={require("./img/calculate1.jpg")}
+            width={200}
+            style={{ paddingBottom: 10 }}
+          />
           <img
             src={require("./img/Screenshot_20191206-020718_Expo.jpg")}
             width={200}
           />
         </div>
-        <p style={{ textAlign: "left" }}>
+        <p className="description" style={{ textAlign: "left" }}>
           I made this app to help calculate my commission at work. Users can
           sign up and create an account or log in with an existing one. User
           authentication is being handled through firebase. You store your work
           load and the app will calculate your commissions for that day. Your
           data is also displayed in graph form with a library called chartjs.
         </p>
-        <p style={{ fontWeight: "bold" }}>USE EXPO AND SCAN BARCODE</p>
+        <p className="description" style={{ fontWeight: "bold" }}>
+          USE EXPO AND SCAN BARCODE
+        </p>
         <img src={require("./img/calc-scan.png")} />
       </article>
 
       <article className="project-one">
         <h1 style={{ fontSize: 35 }}>Project 2</h1>
         <div className="project-row">
-          <img src={require("./img/movie3.jpg")} width={200} />
-          <img src={require("./img/movie2.jpg")} width={200} />
-          <img src={require("./img/movie1.jpg")} width={200} />
+          <img
+            src={require("./img/movie3.jpg")}
+            width={200}
+            style={{ paddingBottom: 10 }}
+          />
+          <img
+            src={require("./img/movie2.jpg")}
+            width={200}
+            style={{ paddingBottom: 10 }}
+          />
+          <img
+            src={require("./img/movie1.jpg")}
+            width={200}
+            style={{ paddingBottom: 10 }}
+          />
         </div>
-        <p style={{ textAlign: "left" }}>
+        <p className="description" style={{ textAlign: "left" }}>
           With this app I'm getting the data from a movie api. I fetch the data
           and display it within the app using flatlist horizontal. You can
           select any movie and get present with more detail about the movie and
           watch the trailer. I also added search functionality as well.
         </p>
-        <p style={{ fontWeight: "bold" }}>USE EXPO AND SCAN BARCODE</p>
+        <p className="description" style={{ fontWeight: "bold" }}>
+          USE EXPO AND SCAN BARCODE
+        </p>
         <img src={require("./img/movie-scan.png")} />
       </article>
 
@@ -83,7 +111,7 @@ function App() {
             style={{ paddingBottom: 10 }}
           />
         </div>
-        <p>
+        <p className="description">
           <a
             href="https://stoic-panini-dd5afc.netlify.com"
             style={{ fontWeight: "bold" }}
@@ -91,7 +119,7 @@ function App() {
             Link To Website
           </a>
         </p>
-        <p style={{ textAlign: "left" }}>
+        <p className="description" style={{ textAlign: "left" }}>
           This React site is a simple survey. The answers are being stored in
           firebase and retrieved to be displayed in graphs. The site is also
           mobile friendly so you can take the survey via mobile.
